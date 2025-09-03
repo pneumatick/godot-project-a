9bb533220b80f1c121b5f6632e9543f2735dfe5c
pneumatick <jhwwefan@gmail.com>
2025-09-03 11:20:24 -0400

    Remove bug where thrown weapons could be damaged twice client-side

--- Scripts/Items/Weapons/thrown_weapon.gd
+++ Scripts/Items/Weapons/thrown_weapon.gd
@@ -16,6 +16,7 @@ func _on_collection_area_body_entered(body: Node3D) -> void:
 		#parent.get_parent().call_deferred("remove_child", parent)
 		# Free weapon object scene
 		#queue_free()
+		
 		# Add weapon to the player that entered the collection area
 		body.call_deferred("add_item", parent)
 		parent.prev_owner = body
@@ -33,11 +34,13 @@ func set_new_owner(new_owner: CharacterBody3D):
 	parent.prev_owner = new_owner
 
 func _apply_damage(damage: int) -> void:
-	var root_node = get_parent()
-	if root_node.condition - damage <= 0:
-		if root_node is Throwable:
-			root_node.explode()
+	if not multiplayer.is_server():
+		return
+	
+	if parent.condition - damage <= 0:
+		if parent is Throwable:
+			parent.explode()
 		else:
-			root_node.queue_free()
+			parent.queue_free()
 	else:
-		root_node.condition -= damage
+		parent.condition -= damage
